3efbb06f152f2cd87cc46e15322c7c4d58c81be8
Allister Maguire <allister@maguir.es>
2026-06-30 23:23:58 +1200

    Replace WH31 with WH32

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -293,8 +293,8 @@
  * Subset of devices that I have access to and have tested with
  */
 #  define DEVICES                    \
-    DECL(ambientweather_wh31e)       \
     DECL(cavius)                     \
+    DECL(fineoffset_WH25)            \
     DECL(fineoffset_ws80)            \
     DECL(fineoffset_WH51)            \
     DECL(fineoffset_ws85)            \
